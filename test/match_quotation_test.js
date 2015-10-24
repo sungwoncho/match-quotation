b818bcc6c10e16cd083a697e3f2c68269158a95f
Sung Won Cho <mikeswcho@gmail.com>
2015-10-24 18:11:06 +1100

    Add test for multi-line strings

--- test/match_quotation_test.js
+++ test/match_quotation_test.js
@@ -1,4 +1,5 @@
 var matchQuotation = require('../index');
+var fs = require('fs');
 var expect = require('chai').expect;
 
 describe("match_quotation", function(){
@@ -28,5 +29,14 @@ describe("match_quotation", function(){
     var result = matchQuotation(sample, {line:1, cursor: 3});
     expect(result.line).to.equal(null);
     expect(result.cursor).to.equal(null);
+  });
+
+  it("matches quotations for multi-line strings", function(){
+    var sample = fs.readFileSync(
+      'test/fixtures/multiline.js',{encoding: 'utf-8'});
+
+    var result = matchQuotation(sample, {line:1, cursor: 11});
+    expect(result.line).to.equal(2);
+    expect(result.cursor).to.equal(6);
   });
 });
